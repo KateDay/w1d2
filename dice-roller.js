aed1f160a655b0a112c49c3e8873d8bcd5be64db
Kate <khummle@gmail.com>
2019-05-01 00:27:31 +0000

    debugging errors fixed

---
+++ dice-roller.js
@@ -0,0 +1,18 @@
+var allArgs = process.argv;
+var justRoll = allArgs[2];
+
+console.log(justRoll);
+
+let dice = function(number) {
+
+
+};
+
+
+
+// Write a program that takes a number from the command line,
+//  and rolls that many six-sided dice.
+//  For example, the following is a sample output of the program.
+   // node dice-roller.js 3
+   // Rolled 3 dice: 2, 6, 5
+//To generate a random number, use Math.random.
